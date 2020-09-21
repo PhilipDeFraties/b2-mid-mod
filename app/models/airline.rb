1388b144b0d37b22b915f9d10e7dfd12c7509af9
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-21 10:47:29 -0600

    Add airline show spec, view page with passenger names, controller, passenger relationship

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,9 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def uniq_passengers
+    passengers.distinct
+  end
 
 end
--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -12,22 +12,22 @@ RSpec.describe 'Airline show page' do
       @passenger_1 = Passenger.create(name: 'Phil')
       @passenger_2 = Passenger.create(name: 'Jade')
       @passenger_3 = Passenger.create(name: 'Robert')
-      @passenger_4 = Passenger.create(name: 'Billy')
-      @passenger_5 = Passenger.create(name: 'Hannah')
-      @passenger_6 = Passenger.create(name: 'Sean')
 
       @flight_passenger_1 = @flight_1.flight_passengers.create(passenger: @passenger_1)
       @flight_passenger_1 = @flight_1.flight_passengers.create(passenger: @passenger_2)
       @flight_passenger_1 = @flight_2.flight_passengers.create(passenger: @passenger_3)
-      @flight_passenger_1 = @flight_2.flight_passengers.create(passenger: @passenger_4)
-      @flight_passenger_1 = @flight_3.flight_passengers.create(passenger: @passenger_5)
-      @flight_passenger_1 = @flight_3.flight_passengers.create(passenger: @passenger_6)
+      @flight_passenger_1 = @flight_2.flight_passengers.create(passenger: @passenger_1)
+      @flight_passenger_1 = @flight_3.flight_passengers.create(passenger: @passenger_2)
+      @flight_passenger_1 = @flight_3.flight_passengers.create(passenger: @passenger_3)
     end
 
     describe "When I visit an airlines show page" do
       it "I see a unique list of passengers that have flights from that airline" do
         visit "/airlines/#{@airline_1.id}"
-        
+
+        @airline_1.passengers.each do |passenger|
+          expect(page).to have_content(passenger.name, count: 1)
+        end
       end
     end
   end
